f44ce8806e5ec9ba5a70291f54c64ec66b9fd0f3
yamakawak <ky2353@columbia.edu>
2020-12-23 22:41:13 -0800

    rebuilding site Wed Dec 23 22:41:13 PST 2020

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,26 @@
 
 # If a command fails then the deploy stops
 set -e
+rm -rf public
 
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# Go To Public folder
-cd public
+# Gi push on to original
+git add .
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+git push origin master
 
+printf "\033[0;32mAdding to Github pages...\033[0m\n"
+# Go To Public folder
+cp -r public ../kohtaroyamakawa.github.io
+cd ../kohtaroyamakawa.github.io
 # Add changes to git.
 git add .
 
